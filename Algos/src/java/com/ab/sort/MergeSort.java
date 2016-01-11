2185fde96930c84ce621f035fc4c714cd617e6fd
unknown <XX61691@PP75CRNL32.pncbank.com>
2016-01-11 00:31:33 -0500

    Merge sort implementation

--- Algos/src/java/com/ab/sort/MergeSort.java
+++ Algos/src/java/com/ab/sort/MergeSort.java
@@ -26,7 +26,45 @@ public class MergeSort implements Sort {
 	@Override
 	public int[] sort(int[] inArr, int order) {
 		// TODO Auto-generated method stub
-		return null;
+		int []x = recursiveSort(inArr, 0, inArr.length-1);
+		return x;
+	}
+	
+	private int[] recursiveSort(int []inArr, int lo, int high){
+
+		if(high == lo)
+			return new int[]{inArr[lo]};
+		
+		
+		//split to the smallest element = 1
+		
+		int a[] = recursiveSort(inArr, lo, (lo + high)/2);
+		int b[] = recursiveSort(inArr, (lo + high)/2 + 1, high);
+		
+		int []x = new int[a.length + b.length];
+		
+		int indexA = 0, indexB = 0;
+		
+		//sort and merge smaller lists
+		for(int i=0; i<x.length; i++){
+			if(indexA >= a.length){
+				x[i] = b[indexB++];
+				continue;
+			}else if(indexB >= b.length){
+				x[i] = a[indexA++];
+				continue;
+			}
+			
+			if(a[indexA]<b[indexB]){
+				x[i] = a[indexA++];
+			}else{
+				x[i] = b[indexB++];
+			}
+			
+			
+		}
+		
+		return x;
 	}
 
 }
