61d364f0541ff3c8d65b4d80db88be35d33c90e9
unknown <XX61691@PP75CRNL32.pncbank.com>
2016-01-19 14:18:32 -0500

    wiggle sort temp

--- Algos/src/java/com/ab/leetcode/med/WiggleSort.java
+++ Algos/src/java/com/ab/leetcode/med/WiggleSort.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class WiggleSort {
 
 	public void wiggleSort(int[] nums) {
+<<<<<<< HEAD
 		
 		Arrays.sort(nums);
 		
@@ -25,6 +26,28 @@ public class WiggleSort {
 	
 	public static void main(String[] args) {
 		System.out.println(10 % 0);
+=======
+
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 1; i < nums.length; j++) {
+				if (nums[j - 1] > nums[j]) {
+					int temp = nums[j - 1];
+					nums[j - 1] = nums[j];
+					nums[j] = temp;
+				}
+			}
+		}
+		
+		
+		for (int i = 1, j=nums.length - 1; i < j; i+=2,j--) {
+			int temp = nums[j - 1];
+			nums[j - 1] = nums[j];
+			nums[j] = temp;
+			
+			
+		}
+
+>>>>>>> wiggle sort temp
 	}
 
 }
